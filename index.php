4fe501644085d51d8a1a41524a010c3d6a966cb8
morgane-marechal <morgane.marechal@laplateforme.io>
2023-05-02 11:41:28 +0200

    router installed

--- index.php
+++ index.php
@@ -1,5 +1,41 @@
 <?php
 
     require 'vendor/autoload.php';
+    
+    $router = new AltoRouter();
+    
+    $router->setBasePath('/super-week');
+
+
+    $router->map( 'GET', '/', function() {
+        require __DIR__ . '/views/home.php';
+    });
+
+
+    $router->map( 'GET', '/home', function() {
+        require __DIR__ . '/views/home.php';
+    });
+
+
+    // match current request url
+    $match = $router->match();
+
+    // call closure or throw 404 status
+    if( is_array($match) && is_callable( $match['target'] ) ) {
+        call_user_func_array( $match['target'], $match['params'] ); 
+    } else {
+        // no route was matched
+        header( $_SERVER["SERVER_PROTOCOL"] . ' 400 Not Found');
+    }
+
+
+
+
+
+
+
+
+
+
 
 ?>
